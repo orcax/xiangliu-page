36b924edf58ffe4ede2f55ba513fa863e5fbeebb
orcax <siriusxyk@gmail.com>
2015-12-24 21:15:36 -0500

    update namecard

--- js/controller.js
+++ js/controller.js
@@ -10,36 +10,30 @@ myControllers.controller('NavCtrl', ['$scope', '$location',
 
 myControllers.controller('HomeCtrl', ['$scope',
   function($scope) {
-  
   }
 ]);
 
 myControllers.controller('PublicationCtrl', ['$scope',
   function($scope) {
-  
   }
 ]);
 
 myControllers.controller('ResearchCtrl', ['$scope',
   function($scope) {
-  
   }
 ]);
 
 myControllers.controller('TeachCtrl', ['$scope',
   function($scope) {
-  
   }
 ]);
 
 myControllers.controller('StudentCtrl', ['$scope',
   function($scope) {
-  
   }
 ]);
 
 myControllers.controller('MaterialCtrl', ['$scope',
   function($scope) {
-  
   }
 ]);
